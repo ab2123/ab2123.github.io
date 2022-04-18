0613b2098a1055974ae93a1a157f6f315f5566c8
ab2123 <95334723+ab2123@users.noreply.github.com>
2022-04-18 05:36:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var i = 0,
+  text;
+text = "I am Abwerk Gosaye";
+
+function typing() {
+  if (i < text.length) {
+    document.getElementById("text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 90);
+  }
+}
+typing();
